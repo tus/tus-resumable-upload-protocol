1e7c670d43dcaf0ff900fc12ae2ec4d3e05701e8
Felix Geisendörfer <felix@debuggable.com>
2013-04-22 12:51:16 +0200

    Implement toc and some styles

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// This code renders the protocol.md file into html and adds a table of content.
+// It's not pretty, but it gets the job done ; )
+
 var http = require('http');
 var robotskirt = require('robotskirt');
 var fs = require('fs');
@@ -20,7 +23,7 @@ http.createServer(function(req, res) {
 function renderProtocol(cb) {
   var renderer = new robotskirt.HtmlRenderer();
   var header = renderer.header;
-  var toc = [];
+  var toc = '';
 
   var prevLevel = 0;
   var counters = [];
@@ -32,24 +35,35 @@ function renderProtocol(cb) {
     level--;
 
     counters[level-1] = (counters[level-1] || 0) + 1;
-    if (prevLevel > level) {
+    if (level < prevLevel) {
       counters.pop();
     }
+    var prefix = counters.join('.')+'. ';
+    if (level > prevLevel) {
+      toc += '<ol>';
+    } else if (level < prevLevel) {
+      toc += '</ol>';
+    }
+    var id = counters.join('-');
+    toc += '<li><a href="#'+id+'">'+prefix+title+'</a></li>';
     prevLevel = level;
 
-    var prefix = counters.join('.')+' ';
-    toc.push(prefix+title);
-    return header(prefix+title, level);
+    return '<h'+level+' id="'+id+'">'+prefix+title+'</h'+level+'>';
   };
 
-  var md = new robotskirt.Markdown(renderer, ~0);
-  fs.readFile(__dirname + '/README.md', 'utf-8', function(err, markdown) {
-    if (err) {
-      cb(null, html);
-      return;
-    }
-    var html = md.render(markdown);
-    console.log(toc);
-    cb(null, html);
+  var md = new robotskirt.Markdown(renderer);
+  fs.readFile(__dirname + '/protocol.md', 'utf-8', function(err, markdown) {
+    if (err) return cb(err);
+
+    fs.readFile(__dirname + '/style.css', 'utf-8', function(err, css) {
+      if (err) return cb(err);
+
+      var html = md.render(markdown);
+      var result =
+        '<style>'+css+'</style>' +
+        '<div id="container">'+toc+html+'</div>';
+
+      cb(null, result);
+    });
   })
 }
